8b728f64c855551b0a4cf6073fe8046801a8b60f
cris <c.cheuquel01@ufromail.cl>
2025-07-22 23:48:27 -0400

    refactor(model): Se agrega la entidad Rol
    - Relación User - Rol -> @ManyToMany

--- src/main/java/TaskManager/TaskManager/model/User.java
+++ src/main/java/TaskManager/TaskManager/model/User.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @Entity
 @Table(name = "Users")
@@ -25,5 +28,13 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "rol_id")
+    )
+    private Set<Rol> roles = new HashSet<>();
 
 }
